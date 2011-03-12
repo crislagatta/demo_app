0ff6fd355062a01f0d186abb0c4cea4016c6f827
Cristiano Fernandes Lagatta <crislagatta@gmail.com>
2011-03-12 01:12:47 -0300

    Done with the demo app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110304035250) do
+ActiveRecord::Schema.define(:version => 20110312035014) do
+
+# Could not dump table "microposts" because of following Mysql2::Error
+#   Invalid date: BTREE
 
 # Could not dump table "users" because of following Mysql2::Error
 #   Invalid date: BTREE
